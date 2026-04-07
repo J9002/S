c7a849ef13f8a4dab29f9a59fc342fe84021ad11
J9002 <JimmyLightning936@outlook.com>
2026-04-07 20:42:27 +0000

    makey makey

--- update.py
+++ update.py
@@ -1,15 +1,5 @@
-import subprocess
-import os
-
 with open("data.txt", "r") as f:
     data = int(f.read())
     data += 1
-
 with open("data.txt", "w") as f:
     f.write(str(data))
-
-subprocess.run(["git", "config", "user.email", "action@github.com"])
-subprocess.run(["git", "config", "user.name", "GitHub Action"])
-subprocess.run(["git", "add", "data.txt"])
-subprocess.run(["git", "commit", "-m", f"Update counter to {data}"])
-subprocess.run(["git", "push"])
