4501a3ecf063380dab3123972c8e1b376985ab23
J9002 <JimmyLightning936@outlook.com>
2026-04-08 17:50:03 +0000

    automated commit

--- updateAudio.py
+++ updateAudio.py
@@ -1,4 +1,5 @@
 import os
+import subprocess
 with open("audio.txt", "w") as file:
     file.write("")
     with open("audio.txt", "a") as file:
@@ -9,3 +10,8 @@ with open("audio.txt", "w") as file:
                 file.write(f"{content[x]}\n")
             else:
                 file.write(f"{content[x]}")
+def git_push():
+    subprocess.run(["git", "add", "."], check=True)
+    subprocess.run(["git", "commit", "-m", "automated commit"], check=True)
+    subprocess.run(["git", "push", "origin", "main"], check=True)
+git_push()
